3c7ecdeefb5d9e527f1df94b8159d27393e9290f
computer_afjp <computer_afjp@gmail.com>
2025-04-30 10:46:51 +0100

    update to opset 12

--- srcs/export_to_onnx.py
+++ srcs/export_to_onnx.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
-model.export(format="onnx", imgsz=256, opset=11, dynamic=True)
+model.export(format="onnx", imgsz=256, opset=12, dynamic=True)
